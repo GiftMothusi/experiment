28d9cb71b4ed09b65162a64a088cdca5b907be6e
GiftMothusi <giftgabanakgosi@gmail.com>
2025-08-13 17:59:11 +0200

    added animated title component and passed it to about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Hero from "./components/Hero";
+import About from "./components/About";
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
             <Hero />
-            <section className="relative h-dvh w-screen overflow-x-hidden bg-red-400"/>
+            <About/>
         </main>
     );
 };
